f76ad976b936e05e667ed6012a726f06d03837cd
wayne tsao <a578ff6@gmail.com>
2023-11-07 17:42:22 +0800

    Initial Commit

---
+++ MyBasicCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBasicCollectionView
+//
+//  Created by 曹家瑋 on 2023/11/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
